aab6ebf269fb12406bdd1c57e8a40f482364b9ee
gfavre <guillaumefavre56@gmail.com>
2020-02-15 12:15:04 +0100

    ajout service POST

--- src/main/java/com/gfavre/bucketlist/ItemRestController.java
+++ src/main/java/com/gfavre/bucketlist/ItemRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,10 @@ public class ItemRestController {
 	List<Item> itemsByCategory(@RequestParam(value = "category") String category) {
 		return itemRepository.findByCategory(category);
 	}
+	
+	@PostMapping
+	Item newEmployee(@RequestBody Item newItem) {
+	    return itemRepository.save(newItem);
+	  }
 
 }
