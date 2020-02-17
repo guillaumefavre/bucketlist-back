aa8fea1e176efbc4bf61dc223751a28cc85ea7d8
gfavre <guillaumefavre56@gmail.com>
2020-02-17 20:18:56 +0100

    Recherche par catégorie : découpage filtre / projection

--- src/main/java/com/gfavre/bucketlistapp/bucketlist/BucketListServiceImpl.java
+++ src/main/java/com/gfavre/bucketlistapp/bucketlist/BucketListServiceImpl.java
@@ -7,11 +7,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationExpression;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.data.mongodb.core.aggregation.ComparisonOperators;
-import org.springframework.data.mongodb.core.aggregation.MatchOperation;
-import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.aggregation.Fields;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.stereotype.Service;
 
 import com.gfavre.bucketlistapp.item.Item;
@@ -38,13 +39,19 @@ public class BucketListServiceImpl implements BucketListService {
 	}
 	
 	public List<BucketList> searchByCategory(String id, String category) {
+		
+		AggregationExpression filtre = ArrayOperators.Filter.filter("itemsList")
+	             .as("item")
+	             .by(ComparisonOperators.Eq.valueOf("item.category").equalToValue(category));
+		
+		ProjectionOperation projection1 = Aggregation.project("title").and(filtre).as("itemsList").andExclude("_id");
+//		projection1 = projection1.and("itemsList").nested("label");
+		
+//		ProjectionOperation projection2 = Aggregation.project().and("itemsList").nested(Fields.fields("itemsList.label"));
 
-		Aggregation aggregation = Aggregation.newAggregation(
-				Aggregation.project().and(ArrayOperators.Filter.filter("itemsList")
-		             .as("item")
-		             .by(ComparisonOperators.Eq.valueOf("item.category").equalToValue(category)))
-		          .as("itemsList"));
-
+		Aggregation aggregation = Aggregation.newAggregation(projection1);
+		
+		// TODO utiliser unwind
 
 		
 		AggregationResults<BucketList> output = mongoTemplate.aggregate(aggregation, BucketList.class, BucketList.class);
