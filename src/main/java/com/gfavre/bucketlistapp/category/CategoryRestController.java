b926c55a2169ad02d1cf1a7f5b6a17728e278922
gfavre <guillaumefavre56@gmail.com>
2020-02-23 18:40:22 +0100

    Category : service delete

--- src/main/java/com/gfavre/bucketlistapp/category/CategoryRestController.java
+++ src/main/java/com/gfavre/bucketlistapp/category/CategoryRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gfavre.bucketlistapp.exceptions.ResourceNotFoundException;
-import com.gfavre.bucketlistapp.item.Item;
 
 @RestController
 public class CategoryRestController {
@@ -40,6 +41,14 @@ public class CategoryRestController {
 		return categoryRepository.findById(id).map(category -> {
 			category.setLabel(updatedCategory.getLabel());
 			return categoryRepository.save(category);
+		}).orElseThrow(() -> new ResourceNotFoundException("Category not found for id "+id));
+	}
+	
+	@DeleteMapping("/category/{id}")
+	public ResponseEntity<?> deleteCategory(@PathVariable Integer id) {
+		return categoryRepository.findById(id).map(category -> {
+			categoryRepository.delete(category);
+			return ResponseEntity.ok().build();
 		}).orElseThrow(() -> new ResourceNotFoundException("Category not found for id "+id));
 	}
 
