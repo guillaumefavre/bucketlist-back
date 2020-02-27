63f2bb757f578ce6d43957c662b486785710a0da
gfavre <guillaumefavre56@gmail.com>
2020-02-27 18:39:54 +0100

    Relation bidirectionnelle bucketlist / item

--- src/main/java/com/gfavre/bucketlistapp/bucketlist/BucketList.java
+++ src/main/java/com/gfavre/bucketlistapp/bucketlist/BucketList.java
@@ -1,10 +1,16 @@
 package com.gfavre.bucketlistapp.bucketlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import com.gfavre.bucketlistapp.item.Item;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,5 +32,8 @@ public class BucketList {
 	private Integer id;
 	
 	private String title;
+	
+	@OneToMany(mappedBy = "bucketList")
+	private List<Item> items = new ArrayList<Item>();
 
 }
